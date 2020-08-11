13b556588562f0db19779ac9edf30779e63a1c66
Oscar Ortiz-Mondragon <oscarortiz@utexas.edu>
2020-08-11 16:40:17 -0500

    can add books with pages and Authors

--- script.js
+++ script.js
@@ -1,54 +1,47 @@
-// My idea, going to use CSS grid to make cards, each card will be influnced by adding more divs based on how much books are made.
-// Calling a book would show Book cover, pages, Author,genre
-// Adding A Book will require the Book name, as well as any detail you already know.
-// First, lets make the grid.
-// Successfully made a grid, will be easy to adjust it of adding more. Can be done with a .length maybe, can do an array to just get the number value of book names, then create the grid that way.
-let grid = document.querySelector("#container")
-grid.style.cssText = "background-color:red; height: 300px; display:grid; grid-template-rows: repeat(2,1fr); grid-template-columns: repeat(2,1fr); grid-gap: 10px; justify-Content: center; text-align:center; align-items:center"
-for(let i= 0; i < 4; i++){
-let a = document.createElement('div');
- a.style.cssText = "display:grid"
- a.className = "grid"
-grid.appendChild(a)
-}
+let myLibrary = ["Harry Potter", "Pride and Prejudice", "To Kill A MonkeyBird", "The Alchemist", "Case Closed",
+];
+let myAuthor = ["JK ROWLING","Jane Austin","Harper Lee","The Science","Phoenix Wright"]
+let myPage = ["500","300","200","90","50"]
+
 function Book(author, title, pages) {
   this.author = author
   this.title = title
   this.pages = pages
 }
-
-let add = document.getElementById("Add")
-add.onclick = function(){
-let promptTitle = prompt("Title Name")
-let promptAuthor = prompt("Author")
-let promptPages = prompt("Page number")
-let newBook = new Book(`${promptTitle}`,`${promptAuthor}`,`${promptPages}`)
-for(let i = 0; i < 4; i++){
-container = document.querySelector("container")
-div = document.getElementsByClassName("grid")
-  if( div.textContent == ""){
-  div[i].textContent = `${newBook.author}`
-  }
-  
-  else if (div.textContent != ""){
-    div[i++].textContent = `${newBook.author}`
-  }
-}
-}
-
-
-
+let add = document.querySelector("input")
 
 
 function addBookToLibrary() {
-  // do stuff here
+container.innerHTML = ""
+// Can i just use the values of input Text?
+let title = prompt("book Title Please!")
+let author = prompt("author Name")
+let pages = prompt("page number")
+myLibrary.push(title)
+myAuthor.push(author)
+myPage.push(pages)
+for(let i = 0; i < myLibrary.length; i++){ 
+  div = document.createElement('div')
+  div.style.cssText = "display:flex; margin: 5px; border: black 10px solid; background-color: blue; text-align: center; justify-content: center; align-items: center;"
+  div.textContent = myLibrary[i] + ", " + myAuthor[i]+ ", " + myPage[i]
+  container.appendChild(div)
+  }
+  
 }
 
 
-let myLibrary = {author: "JK Rowling",
-Title: "Harry Potter"
-};
+add.onclick = function(){
+  addBookToLibrary()
+}
 
 
 
+let container = document.querySelector("#container")
+container.style.cssText = "display:flex; flex-direction: column;"
 
+for(let i = 0; i < myLibrary.length; i++){ 
+div = document.createElement('div')
+div.style.cssText = "display:flex; margin: 5px; border: black 10px solid; background-color: blue; text-align: center; justify-content: center; align-items: center;"
+div.textContent = myLibrary[i] + ", " + myAuthor[i]+ ", " + myPage[i]
+container.appendChild(div)
+}
