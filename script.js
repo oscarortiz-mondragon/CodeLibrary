610e3975c6f017c998ce79b32ba0b5ef2fca81af
Oscar Ortiz-Mondragon <oscarortiz@utexas.edu>
2020-08-12 22:50:18 -0500

    added remove button, no function, added the first stages of added by text

--- script.js
+++ script.js
@@ -1,47 +1,82 @@
-let myLibrary = ["Harry Potter", "Pride and Prejudice", "To Kill A MonkeyBird", "The Alchemist", "Case Closed",
-];
-let myAuthor = ["JK ROWLING","Jane Austin","Harper Lee","The Science","Phoenix Wright"]
-let myPage = ["500","300","200","90","50"]
+// Example with six books
+let myLibrary = 
+["Harry Potter", "Pride and Prejudice", "To Kill A MonkeyBird", "The Alchemist", "Case Closed"];
 
-function Book(author, title, pages) {
+let myAuthor = 
+["JK ROWLING","Jane Austin","Harper Lee","The Science","Phoenix Wright"];
+
+let myPage = 
+["500","300","200","90","50"];
+
+let container = document.querySelector("#container")
+
+container.style.cssText = "display:flex; flex-direction: column;"
+
+for(let i = 0; i < myLibrary.length; i++)
+{ 
+  div = document.createElement('div')
+  let remove = document.createElement("input")
+  remove.className = "remove"
+  remove.setAttribute("type","button")
+  remove.setAttribute("value","remove")
+  div.style.cssText = "display:flex; margin: 5px; padding: 10px; border: black 10px solid; background-color: blue; text-align: center; justify-content: center; align-items: center; font-size: 30px"
+ 
+  div.textContent = myLibrary[i] + ", " + myAuthor[i]+ ", " + myPage[i]
+  container.appendChild(div)
+  div.appendChild(remove)
+}
+
+// Attempt of Prototype Use
+function Book(author, title, pages) 
+{
   this.author = author
   this.title = title
   this.pages = pages
 }
-let add = document.querySelector("input")
+
+// Adding Books to Container
+let add = document.getElementById("Add")
 
 
-function addBookToLibrary() {
-container.innerHTML = ""
-// Can i just use the values of input Text?
-let title = prompt("book Title Please!")
-let author = prompt("author Name")
-let pages = prompt("page number")
-myLibrary.push(title)
-myAuthor.push(author)
-myPage.push(pages)
-for(let i = 0; i < myLibrary.length; i++){ 
-  div = document.createElement('div')
-  div.style.cssText = "display:flex; margin: 5px; border: black 10px solid; background-color: blue; text-align: center; justify-content: center; align-items: center;"
-  div.textContent = myLibrary[i] + ", " + myAuthor[i]+ ", " + myPage[i]
-  container.appendChild(div)
-  }
+function addBookToLibrary() 
+{
   
+  container.innerHTML = ""
+
+  
+
+  let title = prompt("book Title Please!")
+  let author = prompt("author Name")
+  let pages = prompt("page number")
+
+  myLibrary.push(title)
+  myAuthor.push(author)
+  myPage.push(pages)
+
+  for(let i = 0; i < myLibrary.length; i++)
+    { 
+      div = document.createElement('div')
+      let remove = document.createElement("input")
+      remove.className = "remove"
+      remove.setAttribute("type","button")
+      remove.setAttribute("value","remove")
+      div.style.cssText = "display:flex; margin: 5px; padding: 10px; border: black 10px solid; background-color: blue; text-align: center; justify-content: center; align-items: center; font-size: 30px"
+     
+      div.textContent = myLibrary[i] + ", " + myAuthor[i]+ ", " + myPage[i]
+      container.appendChild(div)
+      div.appendChild(remove)
+      
+    } 
+    
 }
 
-
-add.onclick = function(){
+add.onclick = function()
+{
   addBookToLibrary()
 }
 
+// Remove Book
+let remove = document.getElementById("Remove")
 
 
-let container = document.querySelector("#container")
-container.style.cssText = "display:flex; flex-direction: column;"
 
-for(let i = 0; i < myLibrary.length; i++){ 
-div = document.createElement('div')
-div.style.cssText = "display:flex; margin: 5px; border: black 10px solid; background-color: blue; text-align: center; justify-content: center; align-items: center;"
-div.textContent = myLibrary[i] + ", " + myAuthor[i]+ ", " + myPage[i]
-container.appendChild(div)
-}
