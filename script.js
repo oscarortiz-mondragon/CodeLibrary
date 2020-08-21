9f84a3134526d688925fe34f48256fe5a90ce2bf
Oscar Ortiz-Mondragon <oscarortiz@utexas.edu>
2020-08-20 23:22:00 -0500

    added a chagne color option to show read or not

--- script.js
+++ script.js
@@ -12,43 +12,7 @@ let container = document.querySelector("#container")
 
 container.style.cssText = "display:flex; flex-direction: column;"
 
-// for(let i = 0; i < myLibrary.length; i++)
-// { 
 
-//   div = document.createElement('div')
-//   div.id =  [i]
-
-//   let remove = document.createElement('i')
-//   remove.className = "fas fa-trash"
-//   remove.id = "remove"
-//   div.style.cssText = "display:flex; margin: 5px; padding: 10px; border: black 10px solid; background-color: blue; text-align: center; justify-content: center; align-items: center; font-size: 30px"
- 
-//   div.textContent = myLibrary[i] + ", " + myAuthor[i]+ ", " + myPage[i] + " pages"
-//   container.appendChild(div)
-//   remove.addEventListener("click", trash => {
-// trash = remove.parentNode
-
-// let trashID = trash.getAttribute("id")
-
-// container.removeChild(trash)
-// myAuthor.splice(trashID, 1)
-// myLibrary.splice(trashID,1)
-// myPage.splice(trashID,1)
-
-//   //  container.removeChild(div)
-//   //   document.getElementById("yourBook" + [i]).remove()
-//   })
-//   div.appendChild(remove)
-  
-// }
-
-// // Attempt of Prototype Use
-// function Book(author, title, pages) 
-// {
-//   this.author = author
-//   this.title = title
-//   this.pages = pages
-// }
 
 // Adding Books to Container
 let add = document.getElementById("Add")
@@ -63,7 +27,14 @@ function addBookToLibrary()
   let title = document.getElementById("Title").value
   let author = document.getElementById("Author").value
   let pages = document.getElementById("Pages").value
-
+for(let i = 0; i < myAuthor.length; i++){
+  if(myLibrary[i] === title ){
+   return alert("please input a new Book")
+  }
+   if(pages % 1 !== 0 ){
+    return alert("please input a number")
+  }
+}
   myLibrary.push(title)
   myAuthor.push(author)
   myPage.push(pages)
@@ -73,12 +44,23 @@ function addBookToLibrary()
     { 
       div = document.createElement('div')
       let remove = document.createElement('i')
-     
+      let read = document.createElement("p")
+     read.className = "ReadOrNot"
       remove.className = "fas fa-trash"
       div.style.cssText = "display:flex; margin: 5px; padding: 10px; border: black 10px solid; background-color: blue; text-align: center; justify-content: center; align-items: center; font-size: 30px"
-     
+     read.setAttribute("style", "Border: 10px solid green;")
       div.textContent = myLibrary[i] + ", " + myAuthor[i]+ ", " + myPage[i]
       container.appendChild(div)
+      console.log(read.getAttribute('style'))
+read.addEventListener("click", changeRead => {
+
+if(read.getAttribute("style") === "Border: 10px solid green;"){
+  read.setAttribute("style","Border: 10px solid red;")
+}
+else{
+  read.setAttribute("style","Border: 10px solid green;")
+}
+})
       remove.addEventListener("click", trash => {
         trash = remove.parentNode
 
@@ -94,10 +76,13 @@ function addBookToLibrary()
       })
       
       div.appendChild(remove)
-      
+      div.appendChild(read)
     } 
     
 }
+
+
+
 
 add.onclick = function()
 {
